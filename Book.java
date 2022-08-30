f8dc2f86171fd1f6573a70afd415ff44c29049d5
JulietteT <Ihbrbh19@mail.ru>
2022-08-30 20:00:21 +0500

    Initial commit

---
+++ Book.java
@@ -0,0 +1,10 @@
+public class Book {
+
+    public static void main(String[] args) {
+        int price = 14_000;
+
+        int miles = price / 20;
+        System.out.println("Бонусные мили:" + miles);
+
+    }
+}
